ecd35389bcf1d297841f479f34c57ab3e27d6f93
Baldo-Lowell <kingbaldo34@gmail.com>
2025-08-13 01:27:37 +0000

    ACT2

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+$Name = "Lowell Jake Baldo";
+$Age = "21";
+$School = "GlobalReciprocal College";
+$Course = "BSIT";
+$Subject = "WEBSYS";
+$Student = True;
+
+echo "Name " . $Name . "<br>";
+echo "Age " . $Age . "<br>";
+echo "School " . $School . "<br>";
+echo "Course " . $Course . "<br>";
+echo "Subject " . $Subject . "<br>";
+echo "Student " . $Student . "<br>";
